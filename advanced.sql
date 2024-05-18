56b95faef947b844a33ce9c5edb6b23b063968c6
krish <ksnjsi@gmail.com>
2024-05-18 17:05:42 +0530

    Update advanced.sql

--- advanced.sql
+++ advanced.sql
@@ -14,21 +14,11 @@ sum(score) over(partition by category, subcategory order by date) as rtotal_scor
 sum(score) over(partition by category, subcategory) as total_score, --total sum for each group-subgroup division
 AVG(power) over(partition by category, subcategory) as avgpower1, --sub group group avg
 AVG(power) over(partition by category, subcategory order by date) as ravgpower2, -- rooling avg based on date
-date
-from assemble 
-order by date;
-
--- practice 2
-select name,
-score,
-category,
-subcategory,
 rank() over(partition by category, subcategory order by date2) as rank1,  --rank over date2
 dense_rank() over(partition by category, subcategory order by date2) as dense_rank1, --rank over date2 but without skipping numbers 
 row_number() over(partition by category, subcategory order by date2) as rownumber1, --number based on cluster
 row_number() over(partition by category, subcategory order by date2, name ASC) as rownumber2, -- for cluster with priority in duplicate
 row_number() over(partition by category, subcategory order by date2, name DESC) as rownumber3, -- with different priority
-date,
-date2
+date
 from assemble 
 order by date;
